fc379b9bf8df1cd91d05135b7fc07cdf142dcea1
Michele Locati <michele@locati.it>
2024-03-14 14:38:10 +0100

    Add one more test

---
+++ test/fake-vies-server/responses/EL@999080536.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'countryCode' => 'EL',
+    'vatNumber' => '999080536',
+    'requestDate' => getRequestDate(),
+    'valid' => true,
+    'requestIdentifier' => '',
+    'name' => 'ATRADIUS CREDITO Y CAUCION SA DE SEGUROS Y REASEGUROS  ΕΛΛΗΝΙΚΟ ΥΠΟΚΑΤΑΣΤΗΜΑ',
+    'address' => 'ΛΕΩΦΟΡΟΣ ΚΗΦΙΣΙΑΣ MONUMENTAL PLAZA ΚΤΙΡΙΟ Γ 44 15125 - ΜΑΡΟΥΣΙ',
+    'traderName' => '---',
+    'traderStreet' => '---',
+    'traderPostalCode' => '---',
+    'traderCity' => '---',
+    'traderCompanyType' => '---',
+    'traderNameMatch' => 'NOT_PROCESSED',
+    'traderStreetMatch' => 'NOT_PROCESSED',
+    'traderPostalCodeMatch' => 'NOT_PROCESSED',
+    'traderCityMatch' => 'NOT_PROCESSED',
+    'traderCompanyTypeMatch' => 'NOT_PROCESSED',
+];

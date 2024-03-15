aa41665b43daf2c2405f1004c5d546a627a385c5
Michele Locati <michele@locati.it>
2024-03-15 15:55:30 +0100

    Check HR control codes

--- src/Format/DE.php
+++ src/Format/DE.php
@@ -2,11 +2,15 @@
 
 namespace VATLib\Format;
 
+use VATLib\Service\Iso7064;
+
 /**
- * https://www.bmf.gv.at/dam/jcr:6c874c4a-9f30-49d3-8da8-b7405f5aa944/BMF_UID_Konstruktionsregeln_Stand_November%202020.pdf
+ * @see https://www.bmf.gv.at/dam/jcr:6c874c4a-9f30-49d3-8da8-b7405f5aa944/BMF_UID_Konstruktionsregeln_Stand_November%202020.pdf
  */
 class DE implements Vies
 {
+    use Iso7064;
+
     /**
      * @var string
      * @private
@@ -98,17 +102,7 @@ class DE implements Vies
      */
     private function checkControlCode($vatNumber)
     {
-        $p = 10;
-        for ($index = 0; $index <= 7; $index++) {
-            $s = $p + (int) $vatNumber[$index];
-            $m = $s % 10;
-            if ($m === 0) {
-                $m = 10;
-            }
-            $p = ($m << 1) % 11;
-        }
-        $r = 11 - $p;
-        $controlCode = $r === 10 ? 0 : $r;
+        $controlCode = $this->mod11_10($vatNumber, 8);
 
         return $vatNumber[8] === (string) $controlCode;
     }
